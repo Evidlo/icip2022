0dd7fd08590a97983c6a156c22123c9a4f6aa1a6
evan <evan@evanw.org>
2022-07-07 16:18:31 -0500

    clean up old code

--- code/db_sweep.py
+++ code/db_sweep.py
@@ -1,110 +1,52 @@
 #!/usr/bin/env python
+# Evan Widloski - 2022-02-01
+# Monte Carlo simulation of algorithm error for various SNR and number of frames
 
-from multiml.observation import get_frames, add_noise
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import rigidregistration
-from multiml.multiml import register
-
-# ----- scene generation -----
-# %% scene
-
-from skimage.data import hubble_deep_field
-scene = hubble_deep_field()[:, :, 0]
+from multiml.observation import test_sequence
 
 # ----- experiment -----
 # %% experiment
 
 def exp(*, dbsnr=None, max_count=None, noise_model, num_frames):
 
-    # randomize drift amount - restrict ck<N
-    frame_size=np.array((250, 250))
-    # drift = np.random.randint((0, 0), frame_size / num_frames)
-    drift = np.array((8, 8))
-
-    frames_clean = get_frames(
-        scene=scene,
+    size = 250
+    # using nested pqdm does weird stuff to RNG
+    np.random.seed(int.from_bytes(open('/dev/urandom', 'rb').read(4), 'big'))
+    drift = np.random.randint(0, size / num_frames, 2)
+    frames_clean, frames = test_sequence(
         drift=drift,
-        resolution_ratio=1,
-        frame_size=frame_size,
-        num_frames=num_frames,
-        start=(0, 0),
-    )
-
-    frames = add_noise(
-        frames_clean,
-        noise_model=noise_model,
         dbsnr=dbsnr,
-        max_count=max_count,
+        num_frames=num_frames,
+        frame_size = (size, size),
+        jitter_std=1,
+        rotation_std=1
     )
 
-    """
-    # reorder axes for rigidregistration
-    frames_rigid = frames.copy()
-    frames_rigid /= frames_rigid.max()
-    frames_rigid = np.moveaxis(frames_rigid, (0, 1, 2), (2, 0, 1))
-
-    # Instantiate imstack object.
-    s=rigidregistration.stackregistration.imstack(frames_rigid)
-    s.getFFTs()
-    # Set Fourier mask
-    s.makeFourierMask(mask='gaussian',n=2)
-    # Calculate image shifts
-    s.findImageShifts(findMaxima='pixel',verbose=False);
-    # Create registered image stack and average
-    s.get_averaged_image()
-
-    # rigidRegistration drift estimate
-    est_rigid = np.array((
-        np.diff(s.shifts_x).mean(),
-        np.diff(s.shifts_y).mean(),
-    ))
-    """
+    from multiml.multiml import register
 
     est_multiml = register(frames, 'down')
-    # remap registered coordinates from [0, size] -> [-size/2, size/2]
-    est_multiml = est_multiml - frame_size * (est_multiml > frame_size // 2)
 
     # compute absolute error
     return {
-        # 'rigid': np.linalg.norm(est_rigid - drift),
-        # 'est_rigid': est_rigid - drift,
-        'multiml': np.linalg.norm(est_multiml - drift),
-        'est_multiml': est_multiml - drift,
+        'abs_error': np.linalg.norm(est_multiml - drift),
     }
 
-from mas.misc import combination_experiment
+from expsweep import experiment
 
-result_gaussian = combination_experiment(
+result_gaussian = experiment(
     exp,
     dbsnr=np.linspace(-40, -15, 10),
     noise_model=['gaussian'],
-    iterations=50,
+    repeat=50,
     num_frames=[20, 30, 40]
 )
-# result_poisson = combination_experiment(
-#     exp,
-#     max_count=np.linspace(10, 25, 50),
-#     noise_model=['poisson'],
-#     iterations=40,
-#     num_frames=[20]
-#     # num_frames=[20]
-# )
 
 # ----- plotting -----
 # %% plot
-
-x = pd.melt(
-    # result_poisson,
-    result_gaussian,
-    id_vars=['dbsnr', 'noise_model', 'num_frames'],
-    # id_vars=['max_count', 'noise_model', 'num_frames'],
-    # value_vars=['rigid', 'multiml'],
-    value_vars=['multiml'],
-    var_name='method',
-    value_name='abserr'
-)
 
 params = {
     'text.usetex' : True,
@@ -115,10 +57,9 @@ plt.rcParams.update(params)
 
 import seaborn as sns
 plot = sns.lineplot(
-    data=x,
+    data=result,
     x="dbsnr",
-    # x="max_count",
-    y="abserr",
+    y="abs_error",
     hue="num_frames",
     style="num_frames",
     marker='o',
@@ -133,7 +74,6 @@ plt.grid(True, which='minor')
 plt.minorticks_on()
 plt.tight_layout()
 plt.margins(x=0)
-# plot.set(yscale='log')
 plt.show()
 
 def saveit():
--- code/method_compare.py
+++ code/method_compare.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
+# Evan Widloski - 2022-02-01
+# Monte Carlo simulation comparing absolute error of algorithms
 
-from multiml.observation import get_frames, add_noise
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -13,17 +14,23 @@ from multiml.observation import test_sequence
 def exp(*, dbsnr, num_frames):
 
     size = 250
+    # using nested pqdm does weird stuff to RNG
+    np.random.seed(int.from_bytes(open('/dev/urandom', 'rb').read(4), 'big'))
     drift = np.random.randint(0, size / num_frames, 2)
     frames_clean, frames = test_sequence(
         drift=drift,
         dbsnr=dbsnr,
         num_frames=num_frames,
         frame_size = (size, size),
+        jitter_std=1,
+        rotation_std=1
     )
 
     # ----- Rigid -----
     # from registration.rigid import register as rigid_register
     # est_rigid = rigid_register(frames)
+    # this one don't work too good
+    # https://github.com/bsavitzky/rigidRegistration/issues/2
 
     # ----- Ginsburg -----
     from registration.ginsburg import register as ginsburg_register
@@ -36,38 +43,29 @@ def exp(*, dbsnr, num_frames):
     # ----- MultiML (ours) -----
     from multiml.multiml import register as multiml_register
     est_multiml = multiml_register(frames)
-    # remap registered coordinates from [0, size] -> [-size/2, size/2]
-    # est_multiml = est_multiml - frame_size * (est_multiml > frame_size // 2)
 
 
     # compute absolute error
     return {
-        'Ginsburg': np.linalg.norm(est_ginsburg - drift),
         # 'Rigid': np.linalg.norm(est_rigid - drift),
+        'Ginsburg': np.linalg.norm(est_ginsburg - drift),
         'Ours': np.linalg.norm(est_multiml - drift),
         'Guizar': np.linalg.norm(est_guizar - drift)
     }
 
-from mas.misc import combination_experiment
+from expsweep import experiment
 
-result = combination_experiment(
+result = experiment(
     exp,
     dbsnr=np.linspace(-40, -15, 10),
-    iterations=50,
-    num_frames=[20]
+    repeat=50,
+    num_frames=[20],
+    merge=True
 )
 
 # ----- plotting -----
 # %% plot
 
-x = pd.melt(
-    result,
-    id_vars=['dbsnr', 'num_frames'],
-    # id_vars=['max_count', 'noise_model', 'num_frames'],
-    value_vars=['Ginsburg', 'Ours', 'Guizar'],
-    var_name='method',
-    value_name='abserr'
-)
 params = {
     'text.usetex' : True,
     'font.size' : 16,
@@ -77,12 +75,11 @@ plt.rcParams.update(params)
 
 import seaborn as sns
 plot = sns.lineplot(
-    data=x,
+    data=result,
     x="dbsnr",
-    # x="max_count",
-    y="abserr",
-    hue="method",
-    style="method",
+    y="result",
+    hue="experiment",
+    style="experiment",
     marker='o',
     palette='bright'
 )
@@ -95,7 +92,6 @@ plt.grid(True, which='minor')
 plt.minorticks_on()
 plt.tight_layout()
 plt.margins(x=0)
-# plot.set(yscale='log')
 plt.show()
 
 def saveit():
